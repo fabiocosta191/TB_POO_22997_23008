3250d7a9f2fe1c2758099227e9f85f896cf51d2d
fabiocosta191 <132665477+fabiocosta191@users.noreply.github.com>
2023-12-05 14:51:51 +0000

    Add files via upload

---
+++ VERSÕES/1.0/Trabalho_POO_22997_23008_Fase1/TrabalhoPraticoPOO_22997_23008/DLL - Imovel/ClasseMorada.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  Class Morada
+ *	Autor: Fábio Rafael Gomes Costa || Luis Pedro Pereira Freitas
+ *	Contato: a22997@alunos.ipca.pt ||a23008@alunos.ipca.pt
+ *	Data: 01/11/2023
+*/
+using System;
+
+namespace DLLPessoas
+{
+    public class ClasseMorada
+    {
+        #region Estado
+        private string rua;
+        private string codPostal;
+        private string idPredial;
+        private string freguesia;
+        private int nPorta;
+        #endregion
+
+        #region COMPORTAMENTO
+
+        #region CONSTROTORES
+
+        #endregion
+        #region PROPRIEDADES
+
+        #endregion
+        #endregion
+
+    }
+}
